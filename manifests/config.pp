f5a6f5c32ebafbcc8ce207af5ef8d6efe66bf2db
jordiprats <jordi.prats@gmail.com>
2020-11-15 20:26:43 +0100

    align

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,10 +2,10 @@ class threeproxy::config inherits threeproxy {
 
   #/etc/3proxy.cfg
   file { '/etc/3proxy.cfg':
-    ensure => 'present',
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
     content => template("${module_name}/3proxyconf.erb"),
   }
 
